341690f3561db38efd13635c06cbb99569e09819
bekkhus <bekkhus@pop-os.localdomain>
2023-02-15 08:52:33 +0100

    Removed old PokemonInfoTooltip

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,6 +1,6 @@
 import { FunctionComponent } from "react";
 import { BasicPokemon } from "../../models/BasicPokemon";
-import PokemonInfoTooltip from "../PokemonInfoTooltip/PokemonInfoTooltip";
+import PokemonInfoPopover from "../PokemonInfoPopover/PokemonInfoPopover";
 import styles from './ListItem.module.scss';
 
 type ListItemType = {
@@ -11,7 +11,7 @@ const ListItem: FunctionComponent<ListItemType> = ({ item }) => (
     <a className={styles.listItem} id={`tooltip-${item.id}`} href={`https://pokeapi.co/api/v2/pokemon/${item.name}`} key={item.id}>
         <img className={styles.listItemImg} src={item.sprite} />
         <p className={styles.listItemLabel}>{item.name}</p>
-        <PokemonInfoTooltip
+        <PokemonInfoPopover
             resourceId={item.id}
             anchorId={`tooltip-${item.id}`}
             place='left'
