bc6daa3f3ec003e07c630665e4f0821742c75c70
bekkhus <bekkhus@pop-os.localdomain>
2023-02-15 08:47:51 +0100

    Renamed PokemonInfoTooltip to PokemonInfoPopover

---
+++ src/components/PokemonInfoPopover/PokemonInfoPopover.tsx
@@ -0,0 +1,55 @@
+import { FunctionComponent, ReactNode, useState } from "react";
+import { ITooltip, Tooltip as ReactTooltip } from "react-tooltip";
+import useSWR from 'swr';
+import { Pokemon } from "../../models/Pokemon";
+import { getPokemon } from "../../services/pokemonService";
+import styles from './PokemonInfoPopover.module.scss';
+
+type PokemonInfoPopoverProps = {
+    children?: ReactNode;
+    resourceId: number;
+} & ITooltip;
+
+const PokemonInfoPopover: FunctionComponent<PokemonInfoPopoverProps> = ({ resourceId, ...rest }) => {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
+    const { data, isLoading, error } = useSWR<Pokemon>(
+        () => isOpen ? getPokemon.getKey(resourceId) : false,
+        getPokemon.fetcher
+    );
+
+    const renderLoader = () => {
+        let label = '';
+
+        if (error) {
+            label = 'Error';
+        }
+
+        label = isLoading ? 'Loading...' : 'Validating...';
+
+        return <p>{label}</p>;
+    }
+
+    const renderContent = () => (
+        <div className={styles.content} >
+            <img className={styles.img} src={data?.sprites.front_default} />
+            <p className={styles.name}>{data?.name}</p>
+        </div>
+    )
+
+    return (
+        <ReactTooltip
+            className={styles.pokemonInfoPopover}
+            isOpen={isOpen}
+            setIsOpen={setIsOpen}
+            delayHide={100}
+            delayShow={100}
+            clickable
+            {...rest}
+        >
+            {data ? renderContent() : renderLoader()}
+        </ReactTooltip>
+    );
+}
+
+export default PokemonInfoPopover;
